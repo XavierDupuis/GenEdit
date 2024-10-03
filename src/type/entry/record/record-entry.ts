a26d9f285922f32ac6b3de25f66c9b157d37f560
XavierDupuis <github-xavierdupuis@outlook.com>
2024-10-03 17:05:25 -0400

    Delete record-entry.ts

--- src/type/entry/record/record-entry.ts
+++
@@ -1,9 +0,0 @@
-// import { Attributable } from '@type/level-1/attributable';
-// import { RecordId } from '@type/entry/record/record-id';
-// import { Record } from '@type/level-2/entry';
-// import { RecordTag } from '@type/tag/record-tag';
-
-// export interface RecordRecord extends Record, Attributable {
-//     id: RecordId;
-//     tag: RecordTag;
-// }

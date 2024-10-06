0102772b23d6be10da7fbe04d5a96c0d9ec2b918
XavierDupuis <github-xavierdupuis@outlook.com>
2024-10-06 15:32:23 -0400

    typing

--- src/type/tag/ext-attribute-tag.ts
+++ src/type/tag/ext-attribute-tag.ts
@@ -1,4 +1,6 @@
-export type ExtAttributeTag = `_${string}`;
+const ExtTagPrefix = '_';
+
+export type ExtAttributeTag = `${typeof ExtTagPrefix}${string}`;
 
 export const isExtAttributeTag = (value: string): value is ExtAttributeTag =>
-    value.startsWith('_') && value.slice(1) === value.slice(1).toUpperCase();
+    value.startsWith(ExtTagPrefix) && value.slice(1) === value.slice(1).toUpperCase();
